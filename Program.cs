96e2e4003ce3fcd60a0ee836ce4977423be83ae0
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2024-09-24 07:30:54 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExampleOfC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
